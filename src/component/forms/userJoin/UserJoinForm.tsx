3b76463a62e98f740e2138988b83c0a87f64355a
Kofi Boateng <84227601+Kofi-D-Boateng@users.noreply.github.com>
2022-11-26 20:53:30 -0600

    Refactored conditional rendering of room searching. (#30)

--- src/component/forms/userJoin/UserJoinForm.tsx
+++ src/component/forms/userJoin/UserJoinForm.tsx
@@ -1,9 +1,4 @@
-import {
-  ButtonTypeMap,
-  ExtendButtonBase,
-  TextFieldProps,
-  Typography,
-} from "@mui/material";
+import { ButtonTypeMap, ExtendButtonBase, TextFieldProps } from "@mui/material";
 import { FC, FormEvent, MutableRefObject } from "react";
 
 const UserJoinForm: FC<{
@@ -17,29 +12,24 @@ const UserJoinForm: FC<{
   Button: ExtendButtonBase<ButtonTypeMap<{}, "button">>;
 }> = ({ username, TextField, submit, Button, classes, isMobile }) => {
   return (
-    <>
-      <div className={classes.titleContainer}>
-        <Typography variant="h6">Please enter a username</Typography>
-      </div>
-      <form onSubmit={submit}>
-        <TextField
-          className={classes.input}
-          inputRef={username}
-          variant="outlined"
-          size="small"
-          placeholder="enter username"
-          fullWidth
-        />
-        <Button
-          className={classes.btn}
-          variant="outlined"
-          type="submit"
-          fullWidth
-        >
-          Join
-        </Button>
-      </form>
-    </>
+    <form onSubmit={submit}>
+      <TextField
+        className={classes.input}
+        inputRef={username}
+        variant="outlined"
+        size="small"
+        placeholder="enter username"
+        fullWidth
+      />
+      <Button
+        className={classes.btn}
+        variant="outlined"
+        type="submit"
+        fullWidth
+      >
+        Join
+      </Button>
+    </form>
   );
 };
 
